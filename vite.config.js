f3b9281f017d691ecc9999c6d7af48fc0ac01559
rithesh-n <ritheshan18@gmail.com>
2026-01-19 01:16:15 +0530

    Update project configuration and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import markdownPlugin from './src/plugins/markdown.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), markdownPlugin()],
+  assetsInclude: ['**/*.md']
 })
