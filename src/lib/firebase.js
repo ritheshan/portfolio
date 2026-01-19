eefe4c7273dafd0b7e28c29b183d0dc8ec06a694
rithesh-n <ritheshan18@gmail.com>
2026-01-19 17:18:15 +0530

    configured firebase keys

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5PQxGVnpagMVJTUisNV7PnewQxGTTHJ8",
-  authDomain: "portfolio-4f7e2.firebaseapp.com",
-  projectId: "portfolio-4f7e2",
-  storageBucket: "portfolio-4f7e2.firebasestorage.app",
-  messagingSenderId: "770159478435",
-  appId: "1:770159478435:web:fee7f76181888dc3c36116",
-  measurementId: "G-RYWGVPLHYX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
